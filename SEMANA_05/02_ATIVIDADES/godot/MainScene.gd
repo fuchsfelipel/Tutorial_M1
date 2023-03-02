438131fa2828ce25105a97dc415f1fcdb85e73c9
Felipe Liberman Fuchs <felipe@almosthackers.net>
2023-03-02 16:01:50 -0300

    fix code

--- SEMANA_05/02_ATIVIDADES/godot/MainScene.gd
+++ SEMANA_05/02_ATIVIDADES/godot/MainScene.gd
@@ -3,27 +3,22 @@ extends Node2D
 var teste = false
 var valor = 0
 var nome = "Felipe" # Variável criada para uso nos botões 1 e 3, com valor default caso o usuário aperto o botão 3 sem apertar o 1 antes
-# var numero = 0 # Variável pode ser local à função
+var numero = 0 # Variável pode ser local à função
 var lista = [] # Instanciar variável (inserir var)
 
 func _on_Button_pressed():
 	#Coletando dados inseridos pelo usuário
-	#número = int(LineEdit.text) # Pelo o que entendi do exercício, há um erro de lógica. O Objetivo desta função é receber o nome do usuário
-	nome = $LineEdit.text # Remover acento e adicionar cifrão para se referencia a uma variável da UI
-	$Label.text = nome # Exibir nome na Label ao invés da caixa de texto
+	nome = $LineEdit.text # É necessário setar o nome além do número
+	numero = int($LineEdit2.text) # Tirar acento do número e colocar $ na var de UI
+	$Label.text = nome # O texto deve ser mostrado na Label
 
 
 func _on_Button2_pressed():
 	#Incrementando o número inserido pelo usuário
-	
-	# Isso adiciona todos os números em range(10), queremos fazer o append do número à lista
-	# for i in range(10):
-	# 	numero += i # Nomes de variável são case-sensitive
-	# 	lista.append(numero) # Nomes de variável são case-sensitive
-	
-	var numero = int($LineEdit.text) # Declarar a variável numero e adicionar cifrão para se referencia a uma variável da UI
-	lista.append(numero) 
-	$Label.text = str(lista) # Faz mais sentido mostrar todos os números do que apenas 1. Temos que fazer o casting de array para string
+	for i in range(10):
+		numero += i # Consertar nome da variável
+		lista.append(numero) # Consertar nome da variável
+	$Label.text = str(numero)
 
 
 func _on_Button3_pressed():
@@ -39,10 +34,10 @@ func _on_Button3_pressed():
 	# 	if(cont!=0):
 	# 		nome = nome+"baldo"
 	# 	$Label2.text = nome
-	
-	for numero in lista:
-		# Se for par
-		if(numero % 2 != 0):
+	print(lista)
+	for num in lista:
+		# Se for ímpar
+		if(num % 2 != 0):
 			nome += "baldo" # Concatenamos a string "baldo"
 			$Label.text = nome # Não precisamos de uma 2a label então mudei para Label, mas uma outra boa solução seria criar a Label2
 			break
